d964a7e7bd848cd33acd2c7da3baec740a2461a8
Victor <victor.ricart@outlook.com>
2018-10-21 02:25:55 +0200

    Add matches, swipes mock-data

---
+++ database/mock-data/swipes.js
@@ -0,0 +1,101 @@
+const swipe1 = {
+  'base_id': {
+    S: '1'
+  },
+  'target_id': {
+    S: '2'
+  },
+  'timestamp': {
+    S: '1539793960947'
+  },
+  'like': {
+    BOOL: true
+  },
+  'liked': {
+    BOOL: true
+  }
+}
+
+const swipe2 = {
+  'base_id': {
+    S: '2'
+  },
+  'target_id': {
+    S: '1'
+  },
+  'timestamp': {
+    S: '1539793960947'
+  },
+  'like': {
+    BOOL: true
+  },
+  'liked': {
+    BOOL: true
+  }
+}
+
+const swipe3 = {
+  'base_id': {
+    S: '3'
+  },
+  'target_id': {
+    S: '1'
+  },
+  'timestamp': {
+    S: '1539795936533'
+  },
+  'like': {
+    BOOL: true
+  },
+  'liked': {
+    BOOL: false
+  }
+}
+
+const swipe4 = {
+  'base_id': {
+    S: '1'
+  },
+  'target_id': {
+    S: '3'
+  },
+  'timestamp': {
+    S: '1539795936533'
+  },
+  'like': {
+    BOOL: false
+  },
+  'liked': {
+    BOOL: true
+  }
+}
+
+const params = {
+  RequestItems: {
+    'commensal-swipe': [
+      {
+        PutRequest: {
+          Item: swipe1
+        }
+      },
+      {
+        PutRequest: {
+          Item: swipe2
+        }
+      },
+      {
+        PutRequest: {
+          Item: swipe3
+        }
+      },
+      {
+        PutRequest: {
+          Item: swipe4
+        }
+      }
+    ]
+  },
+  ReturnConsumedCapacity: 'TOTAL'
+}
+
+module.exports = {params}
